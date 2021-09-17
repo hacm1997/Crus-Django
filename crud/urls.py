a33ff5396c93840425a292591861413e7dbce9a1
heinermeteoran <heiner@redtic.org>
2021-09-17 01:38:54 -0500

    init13 otra vez

--- crud/urls.py
+++ crud/urls.py
@@ -1,9 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from crud.apps.CrudApp import urls as app_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(app_url))
+    path('', include('apps.CrudApp.urls'))
 ]
